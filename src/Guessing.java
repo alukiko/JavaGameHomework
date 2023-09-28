10c1350752d2de4121f22e58c38ec8e019fa18b5
oklad <ffffdasqwe@gmail.com>
2023-09-28 13:39:25 +1000

    лавлм

---
+++ src/Guessing.java
@@ -0,0 +1,50 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Guessing {
+    public static void main(String[] args) {
+        System.out.println("Привет!\nБудешь угадывать? (да/нет)");
+        Scanner sc = new Scanner(System.in);
+        String answer = sc.nextLine();
+        if (answer.equals("нет")) {
+            System.out.println("(×﹏×)");
+            return;
+        } else if (!answer.equals("да")) {
+            System.out.println("(︶︹︺)\n непонятно, давай до свидания");
+            return;
+        }
+        System.out.println("(⌒‿⌒)");
+        while (true) {
+            int rand = new Random().nextInt(0, 10) + 1;
+            System.out.println("угадай число от 1 до 10");
+            while (true) {
+                int number = sc.nextInt();
+                if (number == rand) {
+                    System.out.println("╰(▔∀▔)╯");
+                    System.out.println("Будешь угадывать? (да/нет)");
+                    sc = new Scanner(System.in);
+                    answer = sc.nextLine();
+                    if (answer.equals("нет")) {
+                        System.out.println("(¬_¬ )");
+                        return;
+                    } else if (!answer.equals("да")) {
+                        System.out.println("(︶︹︺)\n непонятно. Давай, до свидания!");
+                        return;
+                    }
+                    System.out.println("(⌒‿⌒)");
+                    break;
+                } else {
+                    if (number < 1 || number > 10) {
+                        System.out.println("Читать не умеешь?");
+                    } else if (Math.abs(number - rand) > 5) {
+                        System.out.println("Холодно");
+                    } else if (Math.abs(number - rand) > 2) {
+                        System.out.println("Тепло");
+                    } else {
+                        System.out.println("Жгётся!");
+                    }
+                }
+            }
+        }
+    }
+}
